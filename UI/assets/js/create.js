756630630a45a19ea91c859a24209e061c7212e2
frankopkusianwar <okirorfrank3@gmail.com>
2018-10-13 13:59:24 +0300

    This feature is for create store attendant page for the Admin

---
+++ UI/assets/js/create.js
@@ -0,0 +1,16 @@
+function create() {
+    var popup = document.getElementById("createstre");
+    popup.classList.toggle("show");
+}
+function del() {
+    var popup = document.getElementById("actndelete");
+    popup.classList.toggle("show");
+}
+function mkAdmin() {
+    var popup = document.getElementById("mkadmin");
+    popup.classList.toggle("show");
+}
+function notify() {
+    var popup = document.getElementById("notification");
+    popup.classList.toggle("show");
+}
