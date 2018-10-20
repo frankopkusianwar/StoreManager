f88fff9230ee177adcbe7e64ddaa4f15c8ed7724
frankopkusianwar <okirorfrank3@gmail.com>
2018-10-20 18:31:06 +0300

    [#161336913] this is the api feature for getting a specific product

---
+++ backend/functions.py
@@ -0,0 +1,14 @@
+products = []
+class Products(object):
+    def __init__(self):
+        pass
+    def createProduct(self,productName,quantity,price):
+        productItem = {
+            "id": len(products)+1,
+            "productName": productName,
+            "quantity": quantity,
+            "price": price
+        }
+        products.append(productItem)
+        return products
+    
---
+++ backend/products.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request, make_response
+from functions import Products
+
+app = Flask(__name__)
+
+@app.route('/storemanager/api/v1/products', methods=['POST'])
+def postProduct():
+    json_data = request.get_json(force=True)
+    prod = Products()
+    prod.createProduct(json_data['productName'],
+                           json_data['quantity'], 
+                           json_data['price'])
+    return make_response(jsonify({'message': "product created"}), 201)
+
+if __name__ == '__main__':
+    app.run(debug=True)
