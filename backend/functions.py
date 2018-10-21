2712dfc0454e6abb4f61eb1d6985fa58e5470e62
frankopkusianwar <okirorfrank3@gmail.com>
2018-10-21 17:21:19 +0300

    [#161336992] this feature creates sale order

---
+++ backend/functions.py
@@ -0,0 +1,33 @@
+products = []
+sales = []
+
+class Products(object):
+    def __init__(self):
+        pass
+    def createProduct(self,productName,quantity,price):
+        productItem = {
+            "id": len(products)+1,
+            "productName": productName,
+            "quantity": quantity,
+            "price": price
+        }
+        products.append(productItem)
+        return products
+
+class Sales:
+    def __init__(self):
+        pass
+
+    def createSales(self, atendant, productName, quantity, unitPrice):
+        subTotal = int(unitPrice) * int(quantity)
+        records = {
+            "id": len(sales)+1,
+            "atendant": atendant,
+            "product Name": productName,
+            "quantity": quantity,
+            "unit Price": unitPrice,
+            "subTotal": str(subTotal) + "{}".format("ugx")
+
+        }
+        sales.append(records)
+        return sales
