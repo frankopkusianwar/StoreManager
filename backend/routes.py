60fa814d5241d65ee463571cfd03a284ce3eda1f
frankopkusianwar <okirorfrank3@gmail.com>
2018-10-22 17:41:10 +0300

    [#161337053)]creates get sales records by admin

---
+++ backend/routes.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request, make_response, abort
+from functions import Products, products, Sales, sales
+
+app = Flask(__name__)
+
+@app.route('/storemanager/admin/api/v1/products', methods=['POST'])
+def postProduct():
+    json_data = request.get_json(force=True)
+    prod = Products()
+    prod.createProduct(json_data['productName'],
+                           json_data['quantity'], 
+                           json_data['price'])
+    return make_response(jsonify({'message': "product created"}), 201)
+
+@app.route('/storemanager/api/v1/products', methods=['GET'])
+def getProduct():
+    if len(products) == 0:
+        abort(404)
+    return make_response(jsonify({'products':products},200))
+
+@app.route('/storemanager/api/v1/products/<int:productId>', methods=['GET'])
+def getSpecificProduct(productId):
+    prod = [prod for prod in products if prod['id'] == productId]
+    if len(products) == 0:
+        abort(404)
+    return make_response(jsonify({'product':prod[0]},200))
+
+@app.route('/storemanager/atendant/api/v1/sales', methods=['POST'])
+def post_sales_record():
+    json_data = request.get_json(force=True)
+    sal = Sales()
+    sal.createSales(atendant=json_data['atendant'], productName=json_data['productName'],
+                               quantity=json_data['quantity'], unitPrice=json_data['unitPrice'])
+    return make_response(jsonify({'message': 'Sales created successfully'}), 201)
+
+@app.route('/storemanager/admin/api/v1/sales', methods=['GET'])
+def getSales():
+    if len(sales) == 0:
+        abort(404)
+    return make_response(jsonify({'sales':sales},200))
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
